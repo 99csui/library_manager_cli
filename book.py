ab76a29d7894e8e19bbd365ea427ec4fd199e9c8
99Csui <99csui@gmail.com>
2026-06-30 23:08:26 -0400

    fix: change function return and init

--- book.py
+++ book.py
@@ -1,6 +1,6 @@
 class Book:
 
-    def __init__(self, id, title, author, available):
+    def __init__(self, id, title, author):
         self.id = id
         self.title = title
         self.author = author
@@ -10,6 +10,7 @@ class Book:
     def borrow(self):
         if self.available:
             self.available = False
+            return True
         else:
             return False
         
@@ -17,12 +18,19 @@ class Book:
     def return_book(self):
         if not self.available:
             self.available = True
+            return True
         else:
             return False
 
 
     def __str__(self):
-        return f"Book\n Id: {self.id}\nTitle: {self.title}\nAuthor: {self.author}\nAvailable: {self.available}"
+        return (
+            f"Book\n"
+            f"Id: {self.id}\n"
+            f"Title: {self.title}\n"
+            f"Author: {self.author}\n"
+            f"Available: {self.available}"
+        )
 
     def __eq__(self, other):
         if not isinstance(other,Book):
